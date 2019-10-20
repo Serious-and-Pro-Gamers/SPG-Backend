a1c212641456b8215b5d26ef46a28f3509aebbfb
zanderlx <lsaplan97@gmail.com>
2019-10-19 22:45:56 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# SPG-Backend
+# spgsite-webapp backend
+
+## Project virtual environment setup
+```
+$ python3 -m venv env
+```
+
+### Activate virtual environment
+```
+*** Linux ***
+$ source env/bin/activate
+*** Windows ***
+$ env\Scripts\activate
+```
+
+### Install requirements
+```
+(env) $ pip install -r requirements.txt
+```
+
+### Set "debug = True" to reload local changes
+```
+(env) $ sudo vi app.py
+app.run(debug=True)
+```
+
+### Run local instance of Flask app
+```
+(env) $ python app.py
+```
+
+### Deactivate virtual environment
+```
+*** Linux / Windows ***
+(env) $ deactivate
+```
---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/v1/*": {"origins": "*"}})
+
+
+@app.route("/")
+def hello():
+    return "<h1>Welcome to main page for the SeriousAndProGamers API v1.0! (Work In Progress...)</h1><br><p>Use the base url followed by <strong>/v1/&lt;route_path&gt;</strong> to test the API!<p><br><p>Use <strong>/v1</strong> to check the available routes</p>"
+
+
+@app.route("/v1")
+def avail_routes():
+    return "<p>Available routes:<br> GET /ping -- sanity check endpoint</p>"
+
+
+@app.route("/v1/ping")
+def ping():
+    return "pong!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
