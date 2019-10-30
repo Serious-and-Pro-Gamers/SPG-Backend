ede0ab36bdf1566725261a40849d45ee7b2a0244
zanderlx <lsaplan97@gmail.com>
2019-10-29 22:20:26 -0700

    Implemented service module

---
+++ src/services/product_service.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+from models.product import Product
+from database.product_schema import product_schema
+from database.product_schema import products_schema
+
+
+# Get all products
+def get_products():
+    all_products = Product.query.all()
+
+    return product_schema.jsonify(all_products, many=True)
+
+# def get_products():
+#     return "Product service"
