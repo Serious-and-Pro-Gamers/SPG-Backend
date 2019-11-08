49fca2f494837ecbe519603e8201937137001008
zanderlx <lsaplan97@gmail.com>
2019-11-07 22:22:18 -0800

    Use as root app endpoint

--- src/app.py
+++ src/app.py
@@ -1,65 +1,58 @@
 import os
 import json
+from flask_cors import CORS
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_cors import CORS
-from dotenv import load_dotenv
 
 
-# from controllers.product_controller import product_api
-from controllers.product_controller import product_api
-
-# Init app
+# Initialize flask app
 app = Flask(__name__)
+
 
 # Load environment variables
 load_dotenv()
 
-# Database variables
+
+# Get database info from environment variables
 DATABASE_USER = os.getenv("SPG_DATABASE_USER")
 DATABASE_PASSWORD = os.getenv("SPG_DATABASE_PASSWORD")
 DATABASE_ADDRESS = os.getenv("SPG_DATABASE_ADDRESS")
 DATABASE_NAME = os.getenv("SPG_DATABASE_NAME")
-CONNECTION_STRING = "mysql+pymysql://" + DATABASE_USER + ":" + \
-    DATABASE_PASSWORD + "@" + DATABASE_ADDRESS + "/" + DATABASE_NAME
+CONNECTION_STRING = "mysql+pymysql://" + DATABASE_USER + ":" + DATABASE_PASSWORD + "@" + DATABASE_ADDRESS + "/" + DATABASE_NAME
 
-# Database connection
+
+# Set app database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNECTION_STRING
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-# Init db
+# Initialize sqlalchemy
 db = SQLAlchemy(app)
 db.init_app(app)
 
-# Init ma
+
+# Initialize marshmallow
 ma = Marshmallow(app)
 
-# Init cors
+
+# Initialize cors
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
+# Import needs to be here because it has to load the db connection and marshmallow
+from .controllers.product_controller import product_api
 app.register_blueprint(product_api)
 
 
-# Create a product
-# @app.route('/product', methods=['POST'])
-# def add_product():
-#     name = request.json['name']
-#     description = request.json['description']
-#     price = request.json['price']
-#     qty = request.json['qty']
-
-#     new_product = Product(name, description, price, qty)
-
-#     db.session.add(new_product)
-#     db.session.commit()
-
-#     return product_schema.jsonify(new_product)
+# Route for root directory 
+@app.route('/')
+def app_route():
+    return "hello world"
 
 
-# Run server
+# Run development server
 if __name__ == "__main__":
     # Since the host has a wildcard, use the respective urls for dev and prod
     app.run(host='0.0.0.0', debug=True)
